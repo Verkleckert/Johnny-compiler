3b1de1106103535c9cd212f9cb67a7c596dc46f8
Verkleckert <tim.53557@web.de>
2024-04-24 10:17:17 +0200

    Update pylint configuration and remove unused workflow file

--- generator_.py
+++ generator_.py
@@ -67,9 +67,9 @@ def apply_offset(instruction_structure, number_structure, offset):
 def generate_assembly(node):
     ram = {}
     instruction_structure, number_structure = parse_ast(node)
-    print("Instruction Structure:")
+    print("\nInstruction Structure:")
     print(instruction_structure)
-    print("Number Structure:")
+    print("\nNumber Structure:")
     print(number_structure)
     offset = calculate_offset(instruction_structure)
     instruction_structure, number_structure = apply_offset(
--- main.py
+++ main.py
@@ -18,19 +18,20 @@ def print_ast(node, level=0):
             print_ast(child, level + 1)
 
 if __name__ == "__main__":
-    expression = input("Gebe eine Berechnung ein: ")
+    # expression = input("Gebe eine Berechnung ein: ")
     # expression = "(1/(2+3))*(4/(5+6))"
     expression = "12+34-12"
     tokens = tokenize_expression(expression)
+    print("\n\n")
     print("Tokens:")
     print(tokens)
     ast_root = parse_tokens(tokens)
-    print("Abstract Syntax Tree (AST):")
+    print("\nAbstract Syntax Tree (AST):")
     print_ast(ast_root)
 
     instructions = generate_assembly(ast_root)
     # ram.insert(0, generate_data(5, entry_point))
-    print("Assembly Instructions:")
+    print("\nAssembly Instructions:")
     print(instructions)
     
     with open(johnny_path + "parserout.ram", "w") as f:
